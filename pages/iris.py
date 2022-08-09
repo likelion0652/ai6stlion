189796dac3fb2f52d0e0947ca7ca6af19566643d
likelion0652 <105343204+likelion0652@users.noreply.github.com>
2022-08-09 15:46:23 +0900

    Update iris.py

--- pages/iris.py
+++ pages/iris.py
@@ -46,3 +46,5 @@ st.write(iris.target_names[prediction])
 
 st.subheader('Prediction Probability')
 st.write(prediction_proba)
+
+# 출처 : https://github.com/dataprofessor/streamlit_freecodecamp/blob/main/app_7_classification_iris/iris-ml-app.py
